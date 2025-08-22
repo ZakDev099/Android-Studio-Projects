fd7576bfef6d2cf1f4f02c1cea95976d303e9f39
Zak Dyer <30128282@tafe.wa.edu.au>
2025-08-22 20:05:23 +0800

    Created projects

---
+++ ClickMe/app/src/main/java/com/example/clickme/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.clickme
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnClickMe = findViewById<Button>(R.id.btnClickMe)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+
+        btnClickMe.setOnClickListener {
+            tvResult.text = "Button Clicked!!!"
+
+            Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
